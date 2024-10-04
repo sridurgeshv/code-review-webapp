cea61595f753f1e41dc4c3a0dd07bbabfcc3ee6f
learning <sridurgeshv@gmail.com>
2024-10-04 13:54:21 +0530

    added instructions

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # code-review-webapp
+
+## frontend part :
+```bash
+cd client
+npm install react-router-dom @monaco-editor/react socket.io-client axios tailwindcss
+```
+
+### server 
+```bash
+npm install
+npm run start
+```
+
+### client
+```bash
+npm install
+npm start
+```
--- client/src/components/Dashboard/index.js
+++ client/src/components/Dashboard/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './index.css';
 
 function Dashboard() {
   return (
     <div className="p-4">
-      <h1 className="text-2xl font-bold">Dashboard</h1>
+      <h1 className="text-2xl font-bold">Hello Username</h1>
       <div className="mt-4">
         {/* Project list */}
         <p>No projects yet</p>
