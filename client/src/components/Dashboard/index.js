2465dcc46ff64dea4acce5a52ea9f93ad47bf0a0
learning <sridurgeshv@gmail.com>
2024-10-04 13:31:57 +0530

    Initial Commit

--- client/src/components/Dashboard/index.js
+++ client/src/components/Dashboard/index.js
@@ -5,7 +5,7 @@ function Dashboard() {
     <div className="p-4">
       <h1 className="text-2xl font-bold">Dashboard</h1>
       <div className="mt-4">
-        {/* Project list will go here */}
+        {/* Project list */}
         <p>No projects yet</p>
       </div>
     </div>
