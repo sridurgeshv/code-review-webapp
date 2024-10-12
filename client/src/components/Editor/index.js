896af52b20d278086444401cce9fde247ea02c1a
learning <sridurgeshv@gmail.com>
2024-10-12 19:17:33 +0530

    css for buttons

--- client/src/components/Editor/index.js
+++ client/src/components/Editor/index.js
@@ -104,7 +104,7 @@ const Editor = forwardRef(({ user, selectedTemplate = 'javascript', onCodeChange
     <div className="h-screen">
       <div className="editor-toolbar">
         <button onClick={handleSave} className="save-button">
-          <Save size={18} className="icon" />
+          <Save size={16} className="icon" />
           <span className="save-text">Save</span>
       </button>
       </div>
